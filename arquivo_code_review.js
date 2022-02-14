21bcc0e8d96f817ccb4ffdba1b5a0415d2c9b30f
claudiaps <clp.sampedro@gmail.com>
2022-02-14 20:56:19 -0300

    calculadora exemplo para code review

---
+++ arquivo_code_review.js
@@ -0,0 +1,9 @@
+const primeiroNumero = 10;
+const segundoNumero = 20;
+
+const soma = primeiroNumero + segundoNumero;
+const divisao = primeiroNumero / segundoNumero;
+const multiplicacao = primeiroNumero * segundoNumero;
+const subtracao = primeiroNumero - segundoNumero;
+
+console.log({ soma, divisao, multiplicacao, subtracao });
